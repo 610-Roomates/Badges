6223280ea4090ca2999716c327002ddcb1d64f3f
Lê Hoàng <lebaohoangth36@gmail.com>
2024-03-03 13:42:45 +0700

    Create haha.cpp

---
+++ haha.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+bool validEmail(string s)
+{
+    
+}
+// main
+int main(int argc, char const *argv[])
+{
+/*code*/ 
+return 0;
+}
